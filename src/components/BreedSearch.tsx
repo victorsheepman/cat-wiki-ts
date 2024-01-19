60d1800d5fc2c9226e9bce83914ff3b85e94190a
victor_marquez <victorsheepman@gmail.com>
2024-01-19 18:30:02 -0400

    Progress

--- src/components/BreedSearch.tsx
+++ src/components/BreedSearch.tsx
@@ -29,9 +29,11 @@ export const BreedSearch = () => {
         theme={{
             components: {
                 Select: {
-                borderRadius: 59,
-                colorBorder:'unset',
-                colorTextPlaceholder:colorBlack.toString()
+                    borderRadius: 59,
+                    colorBorder:'unset',
+                    colorTextPlaceholder:colorBlack.toString(),
+                    colorPrimaryHover:'#FFF',
+                    optionSelectedBg:"#FFFF"
                 },
             },
         }}
@@ -42,6 +44,9 @@ export const BreedSearch = () => {
         onChange={onChange}
         onSearch={onSearch}
         placeholder="Search"
+        suffixIcon={
+            <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 -960 960 960" width="18"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
+        }
         optionFilterProp="children"
         filterOption={(input, option) => (option?.label ?? '').includes(input)}
         filterSort={(optionA, optionB) =>
@@ -61,14 +66,15 @@ const heroInput = style(
         flexShrink: 0,
         borderRadius: '59px',
         background: colorWhite.toString(),
-        border:'unset',
-        padding:'7px 13px'
+        
     },
     media(
         {minWidth:768},
         {
             width: '294.616px',
             height: '49.67px',
+            padding:'28px',
+            marginTop:'32px'
         }
     ),
     media(
@@ -76,6 +82,7 @@ const heroInput = style(
         {
             width: '394.616px',
             height: '69.67px',
+            marginTop:'52px'
         }
     )
 )
